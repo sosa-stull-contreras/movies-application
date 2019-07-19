bb2c5afd3295f9d4c5eeeb8349388f3273d5ed3e
Alexandra Contreras <alexandra.contreras0903@gmail.com>
2019-07-19 16:11:46 -0500

    update to index

--- src/index.js
+++ src/index.js
@@ -8,13 +8,25 @@ sayHello('World');
  * require style imports
  */
 const {getMovies} = require('./api.js');
+const $ = require('jquery');
+
+
 
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
+  $('#movies').html ("");
+  let moviepost = ""
   movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
+    moviepost +=`<div>id#${id} - ${title} - rating: ${rating}</div>`;
   });
+  $(moviepost).appendTo('#movies')
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
   console.log(error);
 });
+
+
+
+
+const movietitle= $('#movieName').val();
+const movierating=$('#movieRating').val();
