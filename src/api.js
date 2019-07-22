7d67441d1876aaefccab7ecf44376c8be381cb45
Alexandra Contreras <alexandra.contreras0903@gmail.com>
2019-07-22 10:16:34 -0500

    added sort array,description, function

--- src/api.js
+++ src/api.js
@@ -2,10 +2,7 @@ module.exports = {
   getMovies: () => {
     return fetch('/api/movies')
       .then(response => response.json())
-        // .then((movies) => {
-        //   const movieData = movies.filter((movie) =>  {
-        //     console.log(movie)
-        //   })
-        // })
+
   }
 };
+
--- src/index.js
+++ src/index.js
@@ -12,32 +12,35 @@ let movieRating;
 let editRating;
 let deleteMovieData;
 
-const compare = (a, b) => {
-    // Use toUpperCase() to ignore character casing
-    const titleA = a.title.toUpperCase();
-    const titleB = b.title.toUpperCase();
-
-    let comparison = 0;
-    if (titleA > titleB) {
-        comparison = 1;
-    } else if (titleA < titleB) {
-        comparison = -1;
-    }
-    return comparison;
-};
-
-
 
 console.log(getMovies());
+
+
+
 
 //Function populates movie list
 const movies = () =>
     getMovies().then((movies) => {
   $('#movies').html ("");
-  let moviepost = "";
+
+// function that sorts the array alphabetically
+            const sortArray =
+            movies.sort(function (a, b) {
+                if (a.title < b.title){
+                    return -1}
+                else if (a.title > b.title){
+                    return 1;}
+                else{
+                    return 0;}
+            });
+
+
+        let moviepost = "";
   moviepost += '<h2>Here are the movies!</h2>';
-  movies.forEach(({title, rating, id}) => {
+  sortArray.forEach(({title, rating, id}) => {
     moviepost +=`<div>${title} - Rating: ${rating}</div>`;
+      console.log(moviepost)
+
   });
   $(moviepost).appendTo('#movies')
 }).catch((error) => {
@@ -60,45 +63,59 @@ $("select#editRating").change(function(){
   editRating = $(this).children("option:selected").val();
 });
 
-
+console.log(editRating)
 
 //Posts data to the json, updates movies
 $('#submit').click(function (e) {
   e.preventDefault();
   const movieTitle = $('#movieName').val();
-  const userMovies = {
-    title: movieTitle,
-    rating: movieRating
-  };
-  const url = '/api/movies';
-  const options = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(userMovies)
-  };
-  fetch(url, options)
-      .then(movies)
-      .then(editMovies)
-      .then(deleteMovies);
-  $('#movieName').val('');
-  $('#movieRating').val('');
+  if(movieTitle!=='' && movieRating !== 'How many stars do you give this movie?') {
+      let userMovies = {
+          title: movieTitle,
+          rating: movieRating
+      };
+
+      const url = '/api/movies';
+      const options = {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(userMovies)
+      };
+
+      fetch(url, options)
+          .then(movies)
+          .then(editMovies)
+          .then(deleteMovies);
+      $('#movieName').val('');
+      $('#movieRating').val('');
+  }
 });
 
-// $('#editSubmit').attr('disabled', true);
 
 
 //Edit the star rating for movies
 const editMovies = () =>
     getMovies().then((movies) => {
+
+        const sortArray =
+            movies.sort(function (a, b) {
+                if (a.title < b.title){
+                    return -1}
+                else if (a.title > b.title){
+                    return 1;}
+                else{
+                    return 0;}
+            });
+
       // console.log('Here are all the movies:');
       $('#editMovies').html ("");
       let movieEdit = "<option>pick a movie</option>";
-      movies.forEach(({title, rating, id}) => {
-        movieEdit +=`<option value="${title}">${title}</option>`;
+      sortArray.forEach(({title, rating, id}) => {
+        movieEdit +=`<option class="pickMovie" value="${title}">${title}</option>`;
       });
-      $(movieEdit).appendTo('#editMovies').then($("#editSubmit").removeAttr("disabled"))
+      $(movieEdit).appendTo('#editMovies').then($("#editSubmit,#movieDetailSelect").removeAttr("disabled"))
     })
         //gets value for movies option tag
         .then($("select#editMovies").change(function(){
@@ -114,38 +131,113 @@ editMovies();
 //Updates json with new star rating
 $('#editSubmit').click(function (e) {
     e.preventDefault();
-getMovies().then((moviesdata) => {
-    moviesdata.forEach(({title, rating, id}) => {
-        if(editMovie===title){
-            // console.log('i found a match')
-            const userMovies = {
-                 title: editMovie,
-                 rating: editRating
-               };
-               const url = '/api/movies/'+id;
-               const options = {
-                 method: 'PATCH',
-                 headers: {
-                   'Content-Type': 'application/json'
-                 },
-                 body: JSON.stringify(userMovies)
-               };
-               fetch(url, options)
-                   .then(movies)
-                   .then(editMovies)
-                   .then(deleteMovies);
-            }
+    $('#movieDetails').html("");
+
+    const movieTitleEdit = $('#movieNameEdit').val();
+
+    if (movieTitleEdit==='') {
+
+        getMovies().then((moviesdata) => {
+            console.log(moviesdata)
+
+            moviesdata.forEach(({title, rating, id}) => {
+                if (editMovie === title) {
+                    // console.log('i found a match')
+                    const userMovies = {
+                        title: title,
+                        rating: editRating
+                    };
+                    const url = '/api/movies/' + id;
+                    const options = {
+                        method: 'PATCH',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify(userMovies)
+                    };
+                    fetch(url, options)
+                        .then(movies)
+                        .then(editMovies)
+                        .then(deleteMovies);
+                }
+            });
         });
-    });
+    }
+
+    if (movieTitleEdit!=='' && editRating!=="How many stars do you give this movie?"){
+
+        getMovies().then((moviesdata) => {
+            console.log(moviesdata)
+
+            moviesdata.forEach(({title, rating, id}) => {
+                if (editMovie === title) {
+                    // console.log('i found a match')
+                    const userMovies = {
+                        title: movieTitleEdit,
+                        rating: editRating
+                    };
+                    const url = '/api/movies/' + id;
+                    const options = {
+                        method: 'PATCH',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify(userMovies)
+                    };
+                    fetch(url, options)
+                        .then(movies)
+                        .then(editMovies)
+                        .then(deleteMovies);
+                }
+            });
+        });
+    }
+    if (movieTitleEdit!=='' && editRating==="How many stars do you give this movie?"){
+
+        getMovies().then((moviesdata) => {
+            console.log(moviesdata)
+
+            moviesdata.forEach(({title, rating, id}) => {
+                if (editMovie === title) {
+                    // console.log('i found a match')
+                    const userMovies = {
+                        title: movieTitleEdit,
+                        rating: rating
+                    };
+                    const url = '/api/movies/' + id;
+                    const options = {
+                        method: 'PATCH',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify(userMovies)
+                    };
+                    fetch(url, options)
+                        .then(movies)
+                        .then(editMovies)
+                        .then(deleteMovies);
+                }
+            });
+        });
+    }
 });
 
 //Lets you delete a movie
 const deleteMovies = () =>
     getMovies().then((movies) => {
-        // console.log('Here are all the movies:');
+        const sortArray =
+            movies.sort(function (a, b) {
+                if (a.title < b.title){
+                    return -1}
+                else if (a.title > b.title){
+                    return 1;}
+                else{
+                    return 0;}
+            });
+
         $('#deleteMovies').html ("");
         let movieDelete = "<option>pick a movie to delete</option>";
-        movies.forEach(({title, rating, id}) => {
+        sortArray.forEach(({title, rating, id}) => {
             movieDelete +=`<option value="${title}">${title}</option>`;
         });
         $(movieDelete).appendTo('#deleteMovies').then($("#deleteSubmit").removeAttr("disabled"))
@@ -163,6 +255,8 @@ deleteMovies();
 
 //Deletes a movie on submit
 $('#deleteSubmit').click(function (e) {
+    $('#movieDetails').html("");
+
     e.preventDefault();
     getMovies().then((moviesdata) => {
         moviesdata.forEach(({title, rating, id}) => {
@@ -187,3 +281,26 @@ $('#deleteSubmit').click(function (e) {
         });
     });
 });
+
+
+
+// Loads Details of Movie Selected
+
+ $('#movieDetailSelect').click(function (e) {
+     e.preventDefault();
+     const selectMovieEdit = $('#editMovies').val();
+     getMovies().then((movies) => {
+         $('#movieDetails').html(" ");
+         let moviedetails = " ";
+         movies.forEach(({title, rating, id}) => {
+             if (selectMovieEdit === title) {
+                 moviedetails += `<div>${title} Rating: ${rating}</div>`;
+             }
+             console.log(moviedetails)
+         });
+         $(moviedetails).appendTo('#movieDetails')
+     }).catch((error) => {
+         console.log(error);
+     })
+ });
+
